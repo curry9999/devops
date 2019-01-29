23afa24ec248fd32faff3d6fed5941cc48dff83b
curry9999 <mi_ra_cl_e@hotmail.com>
2019-01-29 23:05:32 +0900

    change refactoring

--- _deploy_windows.sh
+++ _deploy_windows.sh
@@ -5,7 +5,7 @@ OPTIONS=$@
 ansible-playbook -i hosts/aws/hosts.ini win_aws.yml -l prd ${OPTIONS}
 test $? -ne 0 && exit 1
 
-ansible-playbook -i hosts/win/ win_ec2.yml -l tag_os_windows ${OPTIONS}
+ansible-playbook -i hosts/win/ win_os.yml -l tag_os_windows ${OPTIONS}
 test $? -ne 0 && exit 1
 
 exit 0
