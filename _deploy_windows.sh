8b491719fab9ca2fa0ff67f7c3a29a63f2c297d5
curry9999 <mi_ra_cl_e@hotmail.com>
2019-01-31 01:25:32 +0900

    change cloudformation template jinja2

--- _deploy_windows.sh
+++ _deploy_windows.sh
@@ -2,6 +2,9 @@
 
 OPTIONS=$@
 
+ansible-playbook -i hosts/aws/hosts.ini gen_cfn_template.yml -l prd ${OPTIONS}
+test $? -ne 0 && exit 1
+
 ansible-playbook -i hosts/aws/hosts.ini win_aws.yml -l prd ${OPTIONS}
 test $? -ne 0 && exit 1
 
