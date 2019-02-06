1a7ab37d85b6eeb493600b68ab082e9002f6ce92
curry9999 <mi_ra_cl_e@hotmail.com>
2019-02-07 00:27:35 +0900

    change

--- _deploy_windows.sh
+++ _deploy_windows.sh
@@ -2,9 +2,6 @@
 
 OPTIONS=$@
 
-ansible-playbook -i hosts/aws/hosts.ini gen_cfn_template.yml -l prd ${OPTIONS}
-test $? -ne 0 && exit 1
-
 ansible-playbook -i hosts/aws/hosts.ini win_aws.yml -l prd ${OPTIONS}
 test $? -ne 0 && exit 1
 
