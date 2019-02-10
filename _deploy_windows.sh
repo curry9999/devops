beb16a4fa3ba0d875e24e5624347192f250822c8
curry9999 <mi_ra_cl_e@hotmail.com>
2019-02-10 18:52:30 +0900

    change Construction

--- _deploy_windows.sh
+++ _deploy_windows.sh
@@ -2,10 +2,10 @@
 
 OPTIONS=$@
 
-ansible-playbook -i hosts/aws/hosts.ini win_aws.yml -l prd ${OPTIONS}
+ansible-playbook -i hosts/prd/hosts.ini win_aws.yml -l aws ${OPTIONS}
 test $? -ne 0 && exit 1
 
-ansible-playbook -i hosts/win/ win_os.yml -l tag_os_windows ${OPTIONS}
+ansible-playbook -i hosts/prd/ win_os.yml -l tag_os_windows ${OPTIONS}
 test $? -ne 0 && exit 1
 
 exit 0
