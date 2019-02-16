8ba086f5b7fe6ce4e985dfc1588a2bcac1675097
curry9999 <mi_ra_cl_e@hotmail.com>
2019-02-17 01:50:37 +0900

    change group vars name

--- _deploy_windows.sh
+++ _deploy_windows.sh
@@ -2,7 +2,7 @@
 
 OPTIONS=$@
 
-ansible-playbook -i hosts/prd/hosts.ini win_aws.yml -l aws ${OPTIONS}
+ansible-playbook -i hosts/prd/hosts.ini win_aws.yml -l aws_win ${OPTIONS}
 test $? -ne 0 && exit 1
 
 ansible-playbook -i hosts/prd/ win_os.yml -l tag_os_windows ${OPTIONS}
