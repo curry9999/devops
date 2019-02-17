fd9ce98e60431486fb8f2662690e2bfe260abae6
curry9999 <mi_ra_cl_e@hotmail.com>
2019-02-17 11:02:36 +0900

    change site yaml name

--- _deploy_windows.sh
+++ _deploy_windows.sh
@@ -2,10 +2,10 @@
 
 OPTIONS=$@
 
-ansible-playbook -i hosts/prd/hosts.ini win_aws.yml -l aws_win ${OPTIONS}
+ansible-playbook -i hosts/prd/hosts.ini aws_win.yml -l aws_win ${OPTIONS}
 test $? -ne 0 && exit 1
 
-ansible-playbook -i hosts/prd/ win_os.yml -l tag_os_windows ${OPTIONS}
+ansible-playbook -i hosts/prd/ os_win.yml -l tag_os_windows ${OPTIONS}
 test $? -ne 0 && exit 1
 
 exit 0
