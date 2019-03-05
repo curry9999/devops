0637cb15af9742b19ece8bb9607cd5619eaa17da
curry9999 <mi_ra_cl_e@hotmail.com>
2019-03-06 00:32:10 +0900

    change cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,23 @@
 import cdk = require('@aws-cdk/cdk');
+import ec2 = require('@aws-cdk/aws-ec2');
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+ constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+   super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+   new ec2.CfnInstance(this, 'MyInstance', {
+       imageId: 'ami-07ad4b1c3af1ea214',
+       instanceType: 't3.micro',
+       keyName: 'kp_wai',
+       monitoring: false,
+       networkInterfaces: [
+         {
+           deviceIndex: "0",
+           associatePublicIpAddress: true,
+           subnetId: 'subnet-09fe229ff896761aa'
+         }
+       ]
+     }
+   );
+ }
 }
