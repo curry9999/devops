15a98e0781dfec5944ba27041ca7176dbc8fdfd5
curry9999 <mi_ra_cl_e@hotmail.com>
2019-03-06 00:06:27 +0900

    add cdk

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/cdk');
+import { CdkStack } from '../lib/cdk-stack';
+
+const app = new cdk.App();
+new CdkStack(app, 'CdkStack');
+app.run();
