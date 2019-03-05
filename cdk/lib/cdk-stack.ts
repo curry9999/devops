d1fca67d0d9afda28d9682ba94e686f5086379c5
curry9999 <mi_ra_cl_e@hotmail.com>
2019-03-06 00:57:56 +0900

    change cdoe

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,19 +5,39 @@ export class CdkStack extends cdk.Stack {
  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
    super(scope, id, props);
 
-   new ec2.CfnInstance(this, 'MyInstance', {
-       imageId: 'ami-07ad4b1c3af1ea214',
-       instanceType: 't3.micro',
-       keyName: 'kp_wai',
-       monitoring: false,
-       networkInterfaces: [
-         {
-           deviceIndex: "0",
-           associatePublicIpAddress: true,
-           subnetId: 'subnet-09fe229ff896761aa'
-         }
-       ]
-     }
-   );
- }
+   for (var i = 1; i <= 2; i++) {
+      new ec2.CfnInstance(this, 'LinuxInstance' + i, {
+          imageId: 'ami-07ad4b1c3af1ea214',
+          instanceType: 't2.micro',
+          keyName: 'kp_wai',
+          monitoring: false,
+          networkInterfaces: [
+            {
+              deviceIndex: "0",
+              associatePublicIpAddress: true,
+              subnetId: 'subnet-09fe229ff896761aa'
+            }
+          ]
+        }
+      );
+    }
+
+    for (var i = 1; i <= 2; i++) {
+      new ec2.CfnInstance(this, 'WindowsInstance' + i, {
+          imageId: 'ami-071e5cd29f6f7eeba',
+          instanceType: 't2.micro',
+          keyName: 'kp_wai',
+          monitoring: false,
+          networkInterfaces: [
+            {
+              deviceIndex: "0",
+              associatePublicIpAddress: true,
+              subnetId: 'subnet-09fe229ff896761aa'
+            }
+          ]
+        }
+      );
+    }
+
+  }
 }
