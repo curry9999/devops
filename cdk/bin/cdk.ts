3cf9d865aaaeceea73ef30a401d967c4bb79e2bd
curry9999 <mi_ra_cl_e@hotmail.com>
2019-03-17 07:58:46 +0900

    add spaces

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,9 +4,9 @@ import { CdkStackEc2 } from '../lib/ec2';
 
 const app = new cdk.App();
 const ec2 = new CdkStackEc2(app, 'prd-ec2-linux');
-ec2.node.apply(new cdk.Tag('env','prd'));
-ec2.node.apply(new cdk.Tag('os','linux'));
-ec2.node.apply(new cdk.Tag('version','2019'));
-ec2.node.apply(new cdk.Tag('Name','prd-linux-ubuntu-1804'));
+ec2.node.apply(new cdk.Tag('env', 'prd'));
+ec2.node.apply(new cdk.Tag('os', 'linux'));
+ec2.node.apply(new cdk.Tag('version', '2019'));
+ec2.node.apply(new cdk.Tag('Name', 'prd-linux-ubuntu-1804'));
 
 app.run();
