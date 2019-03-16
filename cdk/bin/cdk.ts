65603f95e68d43ef2c44de250acf654973498b5f
curry9999 <mi_ra_cl_e@hotmail.com>
2019-03-17 07:44:13 +0900

    add Tags

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 import cdk = require('@aws-cdk/cdk');
 import { CdkStackEc2 } from '../lib/ec2';
-import { Tag } from '@aws-cdk/cdk';
 
 const app = new cdk.App();
 const ec2 = new CdkStackEc2(app, 'prd-ec2-linux');
-ec2.node.apply(new Tag('env','prd'));
-ec2.node.apply(new Tag('os','linux'));
-ec2.node.apply(new Tag('version','2019'));
-ec2.node.apply(new Tag('Name','prd-linux-ubuntu-1804'));
+ec2.node.apply(new cdk.Tag('env','prd'));
+ec2.node.apply(new cdk.Tag('os','linux'));
+ec2.node.apply(new cdk.Tag('version','2019'));
+ec2.node.apply(new cdk.Tag('Name','prd-linux-ubuntu-1804'));
 
 app.run();
