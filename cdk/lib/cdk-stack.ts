e889b68c8f2ef4a2b1a7b8d02bed6ee6366f2718
curry9999 <mi_ra_cl_e@hotmail.com>
2019-03-16 23:38:07 +0900

    change cdk ec2 shrink

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,26 +5,9 @@ export class CdkStack extends cdk.Stack {
  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
    super(scope, id, props);
 
-   for (var i = 1; i <= 2; i++) {
+   for (var i = 1; i <= 1; i++) {
       new ec2.CfnInstance(this, 'LinuxInstance' + i, {
           imageId: 'ami-07ad4b1c3af1ea214',
-          instanceType: 't2.micro',
-          keyName: 'kp_wai',
-          monitoring: false,
-          networkInterfaces: [
-            {
-              deviceIndex: "0",
-              associatePublicIpAddress: true,
-              subnetId: 'subnet-09fe229ff896761aa'
-            }
-          ]
-        }
-      );
-    }
-
-    for (var i = 1; i <= 2; i++) {
-      new ec2.CfnInstance(this, 'WindowsInstance' + i, {
-          imageId: 'ami-071e5cd29f6f7eeba',
           instanceType: 't2.micro',
           keyName: 'kp_wai',
           monitoring: false,
