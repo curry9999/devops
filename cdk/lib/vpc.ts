91a26e30e430b44703e73e505baa24e365f693b3
curry9999 <mi_ra_cl_e@hotmail.com>
2019-03-22 01:53:06 +0900

    change vpc and add securitygroup

--- cdk/lib/vpc.ts
+++ cdk/lib/vpc.ts
@@ -1,13 +1,33 @@
 import cdk = require('@aws-cdk/cdk');
-import vpc = require('@aws-cdk/aws-ec2');
+import ec2 = require('@aws-cdk/aws-ec2');
 
 export class CdkStackVpc extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new vpc.CfnVPC(this, 'VPC', {
-      cidrBlock: '10.1.1.0/24'
+    // Create VPC
+    const vpc = new ec2.VpcNetwork(this, 'VPC', {
+      cidr: '10.0.0.0/21',
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Ingress',
+          subnetType: ec2.SubnetType.Public,
+        },
+        {
+          cidrMask: 24,
+          name: 'Application',
+          subnetType: ec2.SubnetType.Private,
+        },
+      ],
     });
 
+    // Create SecurityGroup
+    const mySecurityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'Allow ssh access to ec2 instances',
+      allowAllOutbound: true   // Can be set to false
+    });
+    mySecurityGroup.addIngressRule(new ec2.AnyIPv4(), new ec2.TcpPort(22), 'allow ssh access from the world');
   }
 }
