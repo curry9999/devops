f142759aa7d70c9662afaa116a247eb410685ab7
curry9999 <mi_ra_cl_e@hotmail.com>
2019-03-22 02:23:02 +0900

    remove ec2 stg

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,17 +5,12 @@ import { CdkStackVpc } from '../lib/vpc';
 
 const app = new cdk.App();
 
-// Stack EC2 stg
-const stg = new CdkStackEc2(app, 'stg-ec2-linux');
-stg.node.apply(new cdk.Tag('env', 'stg'));
-stg.node.apply(new cdk.Tag('Name', 'stg-linux-ubuntu-1804'));
-
-// Stack EC2 prd
+// Stack EC2
 const prd = new CdkStackEc2(app, 'prd-ec2-linux');
 prd.node.apply(new cdk.Tag('env', 'prd'));
 prd.node.apply(new cdk.Tag('Name', 'prd-linux-ubuntu-1804'));
 
-// Stack VPC prd
+// Stack VPC
 const vpc = new CdkStackVpc(app, 'prd-vpc');
 vpc.node.apply(new cdk.Tag('env', 'prd'));
 vpc.node.apply(new cdk.Tag('Name', 'prd-vpc'));
