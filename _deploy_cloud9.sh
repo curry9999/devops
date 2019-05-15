4c5cb967f5d81782cf8b4d53378ddca015703025
Ubuntu <ubuntu@ip-192-168-2-187.ap-northeast-1.compute.internal>
2019-05-15 06:29:31 +0000

    Deploy Cloud9 using aws cdk.

---
+++ _deploy_cloud9.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OPTIONS=$@
+STACK_NAME=prd-cloud9
+CURRENT_DIR=`pwd`
+CDK_DIR=cdk/cloud9/
+
+cd ${CDK_DIR}
+
+npm install
+test $? -ne 0 && exit 1
+
+npm run build
+test $? -ne 0 && exit 1
+
+cdk synth ${STACK_NAME}
+test $? -ne 0 && exit 1
+
+cdk deploy ${STACK_NAME} -f
+test $? -ne 0 && exit 1
+
+exit 0
