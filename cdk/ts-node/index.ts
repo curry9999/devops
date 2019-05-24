68572fcc14c3d6e932eef2f80207d79e74fc9488
Ubuntu <ubuntu@ip-192-168-2-187.ap-northeast-1.compute.internal>
2019-05-24 18:18:34 +0000

    add ts-node

---
+++ cdk/ts-node/index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import cdk = require('@aws-cdk/cdk');
+import iam = require('@aws-cdk/aws-iam');
+
+export class CdkStackIam extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    new iam.CfnGroup(this, 'IamGroup', {
+      groupName: "gruop-curry9999"
+    });
+  }
+}
+
+const app = new cdk.App();
+new CdkStackIam(app, 'prd-iam');
+app.run();
