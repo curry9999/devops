b7fd03a63cef24e6814dfe5b47f0e97a47f3fa47
Ubuntu <ubuntu@ip-192-168-2-187.ap-northeast-1.compute.internal>
2019-07-14 17:42:54 +0000

    move ansible directory

--- exec/_deploy_cdk_linux.sh
+++ exec/_deploy_cdk_linux.sh
@@ -3,6 +3,7 @@
 OPTIONS=$@
 STACK_NAME=prd-ec2-linux
 CURRENT_DIR=`pwd`
+ANSUBLE_DIR=${CURRENT_DIR}/ansible/
 CDK_DIR=cdk/ec2/
 
 cd ${CDK_DIR}
@@ -19,7 +20,7 @@ test $? -ne 0 && exit 1
 cdk deploy ${STACK_NAME} -f --path-metadata false --version-reporting false
 test $? -ne 0 && exit 1
 
-cd ${CURRENT_DIR}
+cd ${ANSUBLE_DIR}
 
 ansible-playbook -i hosts/prd/ os_linux.yml --tags common -l tag_os_linux ${OPTIONS}
 test $? -ne 0 && exit 1
