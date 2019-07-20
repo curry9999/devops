8d7ea70a6f3b646a99ed4a53e2ab0ed23539674c
curry9999 <curry123456789@gmail.com>
2019-07-20 22:40:50 +0900

    Update vpc

--- cdk/vpc/index.ts
+++ cdk/vpc/index.ts
@@ -1,15 +1,43 @@
 #!/usr/bin/env node
-import cdk = require('@aws-cdk/core');
+import autoscaling = require('@aws-cdk/aws-autoscaling');
 import ec2 = require('@aws-cdk/aws-ec2');
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
+import cdk = require('@aws-cdk/core');
 
-export class CdkStackVpc extends cdk.Stack {
-  constructor(scope: cdk.App, id: string , props?: cdk.StackProps) {
-    super(scope, id, props);
+class LoadBalancerStack extends cdk.Stack {
+  constructor(app: cdk.App, id: string) {
+    super(app, id);
 
-    new ec2.Vpc(this, 'Vpc', { maxAzs: 1 });
+    const vpc = new ec2.Vpc(this, 'VPC');
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3_AMD, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(),
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    listener.addTargets('Target', {
+      port: 80,
+      targets: [asg]
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+    asg.scaleOnRequestCount('AModestLoad', {
+      targetRequestsPerSecond: 1
+    });
   }
 }
 
 const app = new cdk.App();
-new CdkStackVpc(app, 'prd-vpc');
+new LoadBalancerStack(app, 'LoadBalancerStack');
 app.synth();
