2ea3f73eeaa855d9299e2368d1a818f2daf0d6f8
curry9999 <curry123456789@gmail.com>
2019-08-04 05:44:33 +0900

    Change tag logic

--- cdk/linux/lib/linux-stack.ts
+++ cdk/linux/lib/linux-stack.ts
@@ -2,28 +2,30 @@ import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2');
 
 export class CdkStackEc2 extends cdk.Stack {
+  private addTag(name: string, value: string) {
+    cdk.Tag.add(this, name, value);
+  };
+
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    cdk.Tag.add(this, 'env', 'prd');
-    cdk.Tag.add(this, 'os', 'linux');
-    cdk.Tag.add(this, 'Name', 'prd-linux-ubuntu-1804');
+    /* Set Tags */
+    this.addTag('env', 'prd');
+    this.addTag('os', 'linux');
+    this.addTag('Name', 'prd-linux-ubuntu-1804');
 
-    const cnt = this.node.tryGetContext("count");
-
-    for (var i = 1; i <= cnt; i++) {
-      new ec2.CfnInstance(this, 'Ec2Instance' + i, {
-          imageId: this.node.tryGetContext("image_id"),
-          instanceType: this.node.tryGetContext("instance_type"),
-          keyName: this.node.tryGetContext("key_pair"),
-          networkInterfaces: [
-            {
-              deviceIndex: '0',
-              associatePublicIpAddress: true
-            }
-          ]
-        }
-      );
-    }
+    /* Create ec2 instance */
+    new ec2.CfnInstance(this, 'Ec2Instance', {
+        imageId: this.node.tryGetContext("image_id"),
+        instanceType: this.node.tryGetContext("instance_type"),
+        keyName: this.node.tryGetContext("key_pair"),
+        networkInterfaces: [
+          {
+            deviceIndex: '0',
+            associatePublicIpAddress: true
+          }
+        ]
+      }
+    );
   }
 }
