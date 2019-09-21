deee999a2e652dc30756ddca59c2b3f7bece916a
curry9999 <curry123456789@gmail.com>
2019-09-22 08:33:03 +0900

    Update exec files

---
+++ exec/_common.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  OPTION="cdk"
+else
+  OPTION=$1
+fi
+
+CURRENT_DIR=`pwd`
+ANSIBLE_DIR=${CURRENT_DIR}/ansible/
+CDK_DIR=cdk/${OS}/
+METADATA=""
+#METADATA="--path-metadata false --version-reporting false"
+
+###############
+# AWS Resource
+###############
+cd ${CDK_DIR}
+
+npm install
+test $? -ne 0 && exit 1
+
+npm run build
+test $? -ne 0 && exit 1
+
+# destroy only
+if [ ${OPTION} = "d" ]; then
+  cdk destroy -f
+  test $? -ne 0 && exit 1
+  exit 0
+fi
+
+cdk synth ${METADATA}
+test $? -ne 0 && exit 1
+
+cdk deploy -f ${METADATA}
+test $? -ne 0 && exit 1
+
+# create and destroy
+if [ ${OPTION} = "x" ]; then
+  cdk destroy -f
+  test $? -ne 0 && exit 1
+  exit 0
+fi
+
+###############
+# OS
+###############
+if [ ${OPTION} = "os" ]; then
+  cd ${ANSIBLE_DIR}
+
+  ansible-playbook -i hosts/prd/ os_${OS}.yml --tags common -l tag_os_${OS}
+  test $? -ne 0 && exit 1
+fi
+
+###############
+# Final
+###############
+exit 0
