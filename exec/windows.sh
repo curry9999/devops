4525abd2caa95feacbf7fd6490eac144bce7efb4
curry9999 <curry123456789@gmail.com>
2019-09-21 21:27:17 +0900

    Update Windows EC2 AMI Version and ansible_python_interpreter

--- exec/windows.sh
+++ exec/windows.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 OPTIONS=$@
+CURRENT_DIR=`pwd`
+ANSIBLE_DIR=${CURRENT_DIR}/ansible/
 
+###############
+# AWS Resource
+###############
+cd ${ANSIBLE_DIR}
 ansible-playbook -i hosts/prd/hosts.ini aws_win.yml -l aws_win ${OPTIONS}
 test $? -ne 0 && exit 1
 
-ansible-playbook -i hosts/prd/ os_win.yml -l tag_os_windows ${OPTIONS}
-test $? -ne 0 && exit 1
+###############
+# OS
+###############
+#cd ${ANSIBLE_DIR}
+#ansible-playbook -i hosts/prd/ os_win.yml -l tag_os_windows ${OPTIONS}
+#test $? -ne 0 && exit 1
 
+###############
+# Final
+###############
 exit 0
